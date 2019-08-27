79d2fdfdded7a0de6093fa1fae40ac970e506238
cgi <varun.mukundhan@gmail.com>
2019-08-27 15:23:59 +0530

    Senario 1 and 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Scenario1Component } from './scenario1/scenario1.component';
+import { Scenario2Component } from './scenario2/scenario2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Scenario1Component,
+    Scenario2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
